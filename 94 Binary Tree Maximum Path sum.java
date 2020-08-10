7ea2cbe4a5d1ca2e0cfa80d261746806e8e62d24
mullaa <48615903+mullaa@users.noreply.github.com>
2020-08-10 19:13:14 -0400

    Update 94 Binary Tree Maximum Path sum.java

--- 94 Binary Tree Maximum Path sum.java
+++ 94 Binary Tree Maximum Path sum.java
@@ -19,6 +19,9 @@ Example 2:
 		
 	Output: 6
     给出一棵二叉树，寻找一条路径使其路径和最大，路径可以在任一节点中开始和结束（路径和为两个节点之间所在路径上的节点权值之和）
+思路：
+利用分治法 解决问题 需要一个变量不断记录带有root点的路径的最大值 curr_max 局部变量 需要另一个变量记录当前的最大路径值 max 类变量 最后返回 当前的最大路径值
+有4种情况要考虑 left + root right + root root left + right + root 不断往上传递的值 只可能是 1， 2， 3中的一种 第四种情况只可能保存在 max里面 而不可能在 curr_max  
 */
 /**
  * Definition of TreeNode:
